c08b9571a2f18d9ed5ca5aa41d44f71a2a915909
temo223 <firstnoobtuber@gmail.com>
2024-05-11 16:27:18 +0400

    Second page
    added second page
    updated js for progress bar

--- main.js
+++ main.js
@@ -24,3 +24,36 @@ let sliderIntervalId = null;
 sliderIntervalId = setInterval(goToNextSlide, 6000);
 
 
+
+
+const targetSection = document.querySelector(".block");
+const bars = document.querySelectorAll(".progress-results");
+
+function showProgress() {
+    for (let i = 0; i < bars.length; i++) {
+        const bar = bars[i];
+        const value = bar.dataset.progress;
+        console.log(value);
+        bar.style.opacity = 1;
+        bar.style.width = `${value}%`;
+    }
+}
+
+function hideProgress() {
+    for (let i = 0; i < bars.length; i++) {
+        const bar = bars[i];
+        bar.style.opacity = 0;
+        bar.style.width = 0;
+    }
+}
+
+window.addEventListener('scroll', () => {
+    const sectionPos = targetSection.getBoundingClientRect().top;
+    const screenPos = window.innerHeight;
+
+    if (sectionPos < screenPos) {
+        showProgress();
+    } else {
+        hideProgress();
+    }
+});
